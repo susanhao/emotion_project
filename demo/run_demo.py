97d63b43246e9a91801eaf753ff4dde7c7241547
susanhao <susanhao@sas.upenn.edu>
2019-08-12 12:04:31 -0700

    nothing much changed

--- demo/run_demo.py
+++ demo/run_demo.py
@@ -18,21 +18,19 @@ face_model = Face_Model(model_dir, 'vggFace_finetune_val_loss_FER.json', 'vggFac
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return "Hello World"
-# def index():
-#     return render_template('index.html')
+def index():
+    return render_template('index.html')
 
-# def gen(camera):
-#     while True:
-#         frame = camera.get_frame(face_model)
-#         yield (b'--frame\r\n'
-#                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+def gen(camera):
+    while True:
+        frame = camera.get_frame(face_model)
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
 
-# @app.route('/video_feed')
-# def video_feed():
-#     return Response(gen(VideoCamera()),
-#                     mimetype='multipart/x-mixed-replace; boundary=frame')
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen(VideoCamera()),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == '__main__':
 	app.run(host='localhost',port = 2000, debug=True)
--- demo/video_cam.py
+++ demo/video_cam.py
@@ -15,18 +15,22 @@ class VideoCamera(object):
 
     # returns camera frames along with bounding boxes and predictions
     def get_frame(self, model):
-        _, fr = self.video.read()
-        gray_fr = cv2.cvtColor(fr, cv2.COLOR_BGR2GRAY)
-        faces = facec.detectMultiScale(gray_fr, 1.3, 5)
+        ret, fr = self.video.read()
 
-        for (x, y, w, h) in faces:
-            fc = gray_fr[y:y+h, x:x+w]
+        if ret == True:
+            gray_fr = cv2.cvtColor(fr, cv2.COLOR_BGR2GRAY)
+            faces = facec.detectMultiScale(gray_fr, 1.3, 5)
 
-            roi = cv2.resize(fc, (48, 48))
-            pred = model.predict_emotion(roi[np.newaxis, :, :, np.newaxis])
+            for (x, y, w, h) in faces:
+                fc = gray_fr[y:y+h, x:x+w]
 
-            cv2.putText(fr, pred, (x, y), font, 1, (255, 255, 0), 2)
-            cv2.rectangle(fr,(x,y),(x+w,y+h),(255,0,0),2)
+                roi = cv2.resize(fc, (48, 48))
+                pred = model.predict_emotion(roi[np.newaxis, :, :, np.newaxis])
 
-        _, jpeg = cv2.imencode('.jpg', fr)
-        return jpeg.tobytes()
+                cv2.putText(fr, pred, (x, y), font, 1, (255, 255, 0), 2)
+                cv2.rectangle(fr,(x,y),(x+w,y+h),(255,0,0),2)
+
+            _, jpeg = cv2.imencode('.jpg', fr)
+            return jpeg.tobytes()
+        else:
+            print ('ERROR')
